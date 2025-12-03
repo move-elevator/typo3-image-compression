e54dc93ff67a392326d2e60aa7d6d7b4c6c65df6
Konrad Michalik <km@move-elevator.de>
2025-12-03 19:01:53 +0100

    fix: update SQL schema to create sys_file table for typo3 v12 support

--- ext_tables.sql
+++ ext_tables.sql
@@ -1,4 +1,11 @@
-CREATE TABLE sys_file_processedfile (
-	compressed tinyint(1) DEFAULT '0' NOT NULL,
-	compress_error text
+CREATE TABLE sys_file
+(
+    compressed     tinyint(1) DEFAULT '0' NOT NULL,
+    compress_error text
+);
+
+CREATE TABLE sys_file_processedfile
+(
+    compressed     tinyint(1) DEFAULT '0' NOT NULL,
+    compress_error text
 );
